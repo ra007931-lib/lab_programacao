5d50e38f895b94cc5e997778a0fed2336e43ee10
José Antonio <007931@libertas.edu.br>
2026-05-25 19:38:41 -0300

    Add files via upload

---
+++ Atividades/ex4.py
@@ -0,0 +1,9 @@
+lista1 = [1,2,3,4,5,6]
+lista2 = [10,20,30,40,50,60]
+
+lista_intercalada = [None] * (len(lista1) + len(lista2))
+
+lista_intercalada[::2] = lista1
+lista_intercalada[1::2] = lista2
+
+print(lista_intercalada)

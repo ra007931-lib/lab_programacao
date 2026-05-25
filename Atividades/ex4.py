abd159bac1a77a9f2692a1dc666d7e0d2b1c1ed8
José Antonio <007931@libertas.edu.br>
2026-05-25 20:03:04 -0300

    Update ex4.py

--- Atividades/ex4.py
+++ Atividades/ex4.py
@@ -1,9 +1,18 @@
-lista1 = [1,2,3,4]
+lista1 = [1,2,3,4,5,6]
 lista2 = [10,20,30,40,50,60]
 
-lista_intercalada = [None] * (len(lista1) + len(lista2))
+if len (lista1) <= len(lista2):
+    menor, maior = lista1, lista2
+else:
+    menor, maior = lista2, lista1
+    
+lista_intercalada = []
 
-lista_intercalada[::2] = lista1
-lista_intercalada[1::2] = lista2
-
-print(lista_intercalada)
+for i in range(len(maior)):
+    if (i < len(menor)):
+        lista_intercalada.append(menor[i])
+    lista_intercalada.append(maior[i])
+    
+print(f"lista1: {lista1}")
+print(f"lista2: {lista2}")
+print(f"lista intercalada: {lista_intercalada}")
